eb50bf1c0af051d9ba01d8bfbad1348c38758752
Eric J Farber <eric@blackandblackcreative.com>
2018-01-31 12:38:09 -0500

    first_slider

--- public/js/app.js
+++ public/js/app.js
@@ -1,8 +1,9 @@
-
 $( document ).ready(function() {
 
 
-  //mobile toggles
+  /////////////////////////////////////////
+ //           mobile toggles            //
+/////////////////////////////////////////
   $('.info-tab').on('click', function(){
     $('.info-tab').toggleClass(  "active" )
 
@@ -10,12 +11,40 @@ $( document ).ready(function() {
     $('.info-body-container').toggleClass('no-display-mobile')
   })
 
-//OPEN TABLE TOGGLE
+  /////////////////////////////////////////
+ //           OPENTABLE TOGGLE          //
+/////////////////////////////////////////
 var $button = $('.js-ot-toggle')
 
 $button.on('click', function (){
   $('.ot-modal__homepage, .ot-modal ').toggleClass('invisible visible')
 } )
+
+  /////////////////////////////////////////
+ //           ROYAL SLIDER              //
+/////////////////////////////////////////
+
+$(document).ready(function($) {
+
+   $(".royalSlider").royalSlider({
+      arrowsNav         : true,
+      keyboardNavEnabled: true,
+      imageScaleMode    : 'fit',
+      controlNavigation : 'bullets',
+      loop              : true,
+      addActiveClass    : true,
+      transitionType    : 'move',
+      slidesSpacing     : 10,
+      controlsInside    : true,
+      visibleNearby     : {
+        enabled: true,
+        centerArea: 0.30
+
+      }
+   });
+});
+
+
 
 
 
